df775a6985e8f65efbe6dcdeb2209d5db5ff8d83
TomasW <tomas.widlak@gmail.com>
2016-05-03 15:31:35 +0200

    shift resource next check date

--- app/Http/Controllers/Project/Resource/DetailController.php
+++ app/Http/Controllers/Project/Resource/DetailController.php
@@ -15,7 +15,9 @@ use App\Model\Currency;
 use App\Model\EshopType;
 use App\Model\Resource;
 use Exception;
+use Illuminate\Support\Facades\DB;
 use Monkey\Breadcrump\BreadcrumbItem;
+use Monkey\DateTime\DateTimeHelper;
 use Monkey\ImportSupport\Project;
 use Monkey\View\ViewFinder;
 
@@ -25,13 +27,13 @@ use Monkey\View\ViewFinder;
  * @author Tomas
  */
 class DetailController extends Controller {
-    
+
     /**
      *
      * @var Project 
      */
     private $project;
-    
+
     /**
      *
      * @var Resource 
@@ -40,48 +42,45 @@ class DetailController extends Controller {
 
     public function getIndex($projectId, $resourceId) {
         $this->project = $project = Project::find($projectId);
-        $this->resource = $resource = $project->getResource( $resourceId );
-        
-        $viewName = 'default.project.resource.detail.'.$resource->codename;
-        if( ViewFinder::existView($viewName) ){
+        $this->resource = $resource = $project->getResource($resourceId);
+
+        $viewName = 'default.project.resource.detail.' . $resource->codename;
+        if (ViewFinder::existView($viewName)) {
             $this->getView()->setBody($viewName);
         }
         $resource->getStateTester();
-        $resourceCurrency = Currency::find( $resource->getResourceStats()->getResourceSetting()->currency_id );
-        $resourceDetail =  $resource->getResourceDetail();
-        
-        if($resourceDetail === null){
+        $resourceCurrency = Currency::find($resource->getResourceStats()->getResourceSetting()->currency_id);
+        $resourceDetail = $resource->getResourceDetail();
+
+        if ($resourceDetail === null) {
             throw new Exception("Missing resource detail for project {$project->id} and resource {$resource->id}");
         }
-        if($resource->id == 4){
-            $this->getView()->addParameter('eshopType', EshopType::find($resourceDetail->eshop_type_id)  );
+        if ($resource->id == 4) {
+            $this->getView()->addParameter('eshopType', EshopType::find($resourceDetail->eshop_type_id));
         }
-        
+
         $stack = null;
-        if(!$resource->isValid()){
+        if (!$resource->isValid()) {
             $stack = $resource->getStack();
         }
-        
+
         $connectionDetail = $resource->getConnectionDetail();
-        
-        
+
         $this->getView()->addParameter('stack', $stack);
         $this->getView()->addParameter('project', $project);
         $this->getView()->addParameter('resource', $resource);
         $this->getView()->addParameter('resourceDetail', $resourceDetail);
         $this->getView()->addParameter('resourceCurrency', $resourceCurrency);
         $this->getView()->addParameter('connectionDetail', $connectionDetail);
-        
+
         $this->prepareMenu($project);
     }
-    
+
     protected function breadcrumbAfterAction($parameters = array()) {
         $breadcrumbs = parent::breadcrumbAfterAction();
-        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('user', 'User', \Monkey\action(UserDetailController::class, ['user_id' => $this->project->user_id ])));
-        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('project', 'Project', \Monkey\action(ProjectDetailController::class, ['project_id' =>$this->project->id ])));
-        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('resource', 'Resource', \Monkey\action(self::class, ['project_id' =>$this->project->id, 'resource_id' => $this->resource->id ])));
+        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('user', 'User', \Monkey\action(UserDetailController::class, ['user_id' => $this->project->user_id])));
+        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('project', 'Project', \Monkey\action(ProjectDetailController::class, ['project_id' => $this->project->id])));
+        $breadcrumbs->addBreadcrumbItem(new BreadcrumbItem('resource', 'Resource', \Monkey\action(self::class, ['project_id' => $this->project->id, 'resource_id' => $this->resource->id])));
     }
-    
-    
 
 }
