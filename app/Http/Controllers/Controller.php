fa6e5f47a0bf034e52a1baaba49833243fb478a1
TomasW <tomas.widlak@gmail.com>
2016-05-05 12:10:13 +0200

    title

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -58,6 +58,7 @@ class Controller extends BaseViewController {
             $invalidProject = $invalidProjectList->getProject($project->id);
             if ($invalidProject) {
                 $menuItem->setName("{$invalidProject->getName()} [{$invalidProject->getInvalidResourceCount()}]");
+                $menuItem->setTitle($project->getId());
                 $menuItem->addClass('invalid');
             }
 
