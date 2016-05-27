e4e3fc6edb923cc6ab919c17ab809ac42ff21d35
TomasW <tomas.widlak@gmail.com>
2016-05-27 08:24:29 +0200

    soundest is without history

---
+++ helpers/Monkey/ImportSupport/Resource/Resources/SoundestResource.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Monkey\ImportSupport\Resource\Resources;
+
+use Monkey\ImportSupport\Resource\ResourceWithoutHistoryV2;
+
+
+
+/**
+ * Description of EshopResource
+ *
+ * @author Tomas
+ */
+class SoundestResource extends ResourceWithoutHistoryV2 {
+    
+    
+    
+    
+    
+}
