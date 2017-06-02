81d965a31b7d8c43f580675242f17a013875bb87
Tomas Widlak <tomas.widlak@monkeydata.com>
2017-06-02 13:14:51 +0200

    project list by resource

---
+++ app/Http/Controllers/ProjectList/ResourcesController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers\ProjectList;
+
+use App\Http\Controllers\Controller;
+use App\Model\Project;
+use App\Model\Resource;
+use Monkey\View\ViewRender;
+
+/**
+ * Description of HomepageController
+ *
+ * @author Tomas
+ */
+class ResourcesController extends Controller {
+    
+    public function getIndex($resource_id = null) {
+        $availableResources = Resource::where("is_public", 1)->get();
+        if($resource_id == null){
+            foreach ($availableResources as $resource){
+                $resource_id = $resource->id;
+                break;
+            }
+        }
+        // vde($availableResources);
+
+        $projects = Project::join("resource_setting_v2 as rs", "project.id", '=', 'rs.project_id')
+            ->where('rs.active', '=', 1)
+            ->where('rs.resource_id', '=', $resource_id)
+            ->select('project.*')
+            ->simplePaginate(30);
+
+
+        ViewRender::addParameter("availableResources", $availableResources);
+        ViewRender::addParameter("currentResourceId", $resource_id);
+
+        ViewRender::addParameter("projects", $projects);// vde("resources");
+    }
+}
