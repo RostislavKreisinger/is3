9a2ef38b4e4764bc629dd46630393754dc56afc3
Tomas Widlak <tomas.widlak@monkeydata.com>
2017-06-19 15:16:26 +0200

    fix active

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -14,7 +14,7 @@ class Project extends Model {
     protected $guarded = [];
     
     public function getResources() {
-        return $this->belongsToMany(Resource::class, "resource_setting", "project_id", "resource_id")->where('active', '!=', 3);
+        return $this->belongsToMany(Resource::class, "resource_setting", "project_id", "resource_id")->where('resource_setting.active', '!=', 3);
     }
     
     public function getResourceSettings($resource_id = null) {
