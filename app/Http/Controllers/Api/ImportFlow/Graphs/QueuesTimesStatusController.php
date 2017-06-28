dc762fc0a855f0672bad5e1a514cbeed46c463bb
Tomas Widlak <tomas.widlak@monkeydata.com>
2017-06-28 14:09:07 +0200

    add deleted at condition

--- app/Http/Controllers/Api/ImportFlow/Graphs/QueuesTimesStatusController.php
+++ app/Http/Controllers/Api/ImportFlow/Graphs/QueuesTimesStatusController.php
@@ -23,21 +23,25 @@ SELECT id, 'import' as `type`, active, TIMESTAMPDIFF(SECOND,DATE_SUB(start_at,IN
 FROM if_import
 WHERE active not in (0,3)
 AND ttl > 0
+AND deleted_at IS NULL
 UNION
 SELECT id, 'etl' as `type`, active, TIMESTAMPDIFF(SECOND,DATE_SUB(start_at,INTERVAL -2 HOUR),NOW()) as run_time
 FROM if_etl
 WHERE active not in (0,3)
 AND ttl > 0
+AND deleted_at IS NULL
 UNION
 SELECT id, 'calc' as `type`, active, TIMESTAMPDIFF(SECOND,DATE_SUB(start_at,INTERVAL -2 HOUR),NOW()) as run_time
 FROM if_calc
 WHERE active not in (0,3)
 AND ttl > 0
+AND deleted_at IS NULL
 UNION
 SELECT id, 'output' as `type`, active, TIMESTAMPDIFF(SECOND,DATE_SUB(start_at,INTERVAL -2 HOUR),NOW()) as run_time
 FROM if_output
 WHERE active not in (0,3)
 AND ttl > 0
+AND deleted_at IS NULL
 ) as u
 GROUP BY u.`type`, u.`active`");
 
