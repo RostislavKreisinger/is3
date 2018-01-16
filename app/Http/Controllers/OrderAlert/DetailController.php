937a0fa08326ecc50e726cd4dbd138147d4493ba
adambardon <adam.bardon@monkeydata.com>
2018-01-16 16:36:41 +0100

    Adds custom configuration of dxdataGrid columns of orders in detail

--- app/Http/Controllers/OrderAlert/DetailController.php
+++ app/Http/Controllers/OrderAlert/DetailController.php
@@ -31,27 +31,57 @@ class DetailController extends BaseController {
             View::share("eshop", $data[0]);
         }
 
-        $ordersRaw = MDOrderAlertConnections::getOrderAlertDwConnection()->table("f_order_eshop_".$storeId)->get();
+        $ordersRaw = MDOrderAlertConnections::getOrderAlertDwConnection()->table("f_order_eshop_".$storeId)
+            ->leftJoin('d_order_status', 'f_order_eshop_'.$storeId.'.status_id', '=', 'd_order_status.id')
+            ->get(['f_order_eshop_'.$storeId.'.*','d_order_status.status', 'd_order_status.title']);
+
         $orders = array();
         foreach ($ordersRaw as $orderRaw) {
             $orderJSON = json_decode($orderRaw->json, true);
             $order = array();
-//            vd($orderJSON);
+            $order['statusCode'] = $orderRaw->status;
+            $order['statusTitle'] = $orderRaw->title;
+//            vd($orderRaw);
             foreach ($orderJSON as $key => $val) {
                 if (is_array($val)) {
                     foreach ($val as $arrayKey => $arrayVal) {
-//                        array_push($order, $arrayItem);
                         $order[$arrayKey] = $arrayVal;
                     }
                 }
                 else {
-//                    array_push($order, ($key => $val);
                     $order[$key] = $val;
                 }
             }
             array_push($orders, $order);
         }
+
+        $visibleColumns = ["number", "priceIncl", "email", "channel", "statusTitle"];
+        $omittedColumns = ["status", "customStatusId", "firstname", "middlename", "lastname"];
+        $columnsConfig = '';
+        if (!empty($orders)) {
+            foreach ($orders[0] as $key => $val) {
+                if (in_array($key, $visibleColumns)) {
+                    if ($columnsConfig != '') {
+                        $columnsConfig .= ', "' . $key . '"';
+                    } else {
+                        $columnsConfig .= '"' . $key . '"';
+                    }
+                }
+                else if (in_array($key, $omittedColumns)) {
+                    continue;
+                }
+                else {
+                    if ($columnsConfig != '') {
+                        $columnsConfig .= ', { dataField: "' . $key . '", visible: false }';
+                    } else {
+                        $columnsConfig .= '{ dataField: "' . $key . '", visible: false }';
+                    }
+                }
+            }
+        }
+        $columnsConfig .= ', { caption: "Customer Name", calculateCellValue: function(data) { return [data.firstname, data.middlename, data.lastname].join(" "); }}';
+//        vd($columnsConfig);
         View::share("orders", $orders);
-        View::share("columns", "");
+        View::share("columns", $columnsConfig);
     }
 }
