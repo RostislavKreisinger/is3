b4a688cde00758622f5280084b11ab9f7295b064
Tomas Widlak <tomas.widlak@monkeydata.com>
2018-05-10 18:20:08 +0200

    font size

--- app/Http/Controllers/Open/ImportFlow/Graph/BaseController.php
+++ app/Http/Controllers/Open/ImportFlow/Graph/BaseController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Open\ImportFlow\Graph;
 
 use App\Http\Controllers\BaseViewController;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Monkey\View\View;
 
 class BaseController extends BaseViewController {
 
     public function getIndex() {
-
+        $fontSize = Input::get("fontSize", '1em');
+        View::share("fontSize", $fontSize);
     }
 
 }
