6b07f057541b4de346ace90b9eee796f0c8fee31
Tomas Widlak <tomas.widlak@monkeydata.com>
2018-05-30 10:31:48 +0200

    UI

--- resources/views/tmp/shoptet-project-stats.blade.php
+++ resources/views/tmp/shoptet-project-stats.blade.php
@@ -1,11 +1,29 @@
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    table td, table th {
+        border: 1px solid #000000;
+        padding: 2px 4px ;
+    }
+</style>
 <table>
+    <tr>
+        <th>#</th>
+        <th>url</th>
+        <th>order count</th>
+        <th>product count</th>
+        <th>product variant count</th>
+
+    </tr>
     @foreach ($projects as $project)
     <tr>
         <td>
-            {{$project->id}}
+            <a href="https://app-support.monkeydata.com/crm/plugins/{{$project->user_id}}" target="_blank">{{$project->id}}</a>
         </td>
         <td>
-            {{$project->weburl}}
+            <a href="{{$project->weburl}}" target="_blank">{{$project->weburl}}</a>
         </td>
         <td>
             {{$project->orders}}
