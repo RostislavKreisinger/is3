0f370da687bd9d9298674689d6969e445468e2c6
Tomas Widlak <tomas.widlak@monkeydata.com>
2018-06-06 08:40:11 +0200

    add resource setting active 3 to project detail

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -15,11 +15,13 @@ class Project extends Model {
     protected $guarded = [];
     
     public function getResources() {
-        return $this->belongsToMany(Resource::class, "resource_setting", "project_id", "resource_id")->where('resource_setting.active', '!=', 3);
+        return $this->belongsToMany(Resource::class, "resource_setting", "project_id", "resource_id"); //->where('resource_setting.active', '!=', 3);
+        // return $this->belongsToMany(Resource::class, "resource_setting", "project_id", "resource_id")->where('resource_setting.active', '!=', 3);
     }
     
     public function getResourceSettings($resource_id = null): HasMany {
-        $builder = $this->hasMany(ResourceSetting::class)->where('active', '!=', 3);
+        $builder = $this->hasMany(ResourceSetting::class);
+        //$builder = $this->hasMany(ResourceSetting::class)->where('active', '!=', 3);
         if($resource_id){
             $builder->where('resource_id', '=', $resource_id);
         }
