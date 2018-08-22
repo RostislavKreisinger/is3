62d830eb439bfbb8fd8da3ab90667d43624a9e8e
lukas <lukas.kielar@monkeydata.com>
2018-08-22 10:10:42 +0200

    Reworked StuckFlowsController to extend AFlowsController.

--- app/Http/Controllers/Open/ImportFlow/Table/StuckFlowsController.php
+++ app/Http/Controllers/Open/ImportFlow/Table/StuckFlowsController.php
@@ -3,22 +3,13 @@
 namespace App\Http\Controllers\Open\ImportFlow\Table;
 
 
-use App\Http\Controllers\Project\DetailController;
-use App\Http\Controllers\Project\Resource\DetailController as ResourceDetailController;
-use App\Model\ImportPools\IFCalcPool;
-use App\Model\ImportPools\IFEtlPool;
-use App\Model\ImportPools\IFImportPool;
-use App\Model\ImportPools\IFOutputPool;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Routing\Controller;
 use Monkey\DateTime\DateTimeHelper;
 
 /**
  * Class StuckFlowsController
  * @package App\Http\Controllers\Open\ImportFlow\Table
  */
-class StuckFlowsController extends Controller {
+class StuckFlowsController extends AFlowsController {
     /**
      * @return array
      */
@@ -27,36 +18,15 @@ class StuckFlowsController extends Controller {
             ->minusHours(12)->mysqlFormat();
         $results = [];
 
-        array_push($results, ...$this->prepareBuilder(IFImportPool::whereOlderThan($time))->get());
-        array_push($results, ...$this->prepareBuilder(IFEtlPool::whereOlderThan($time))->get());
-        array_push($results, ...$this->prepareBuilder(IFCalcPool::whereOlderThan($time))->get());
-        array_push($results, ...$this->prepareBuilder(IFOutputPool::whereOlderThan($time))->get());
+        foreach (static::IF_STEP_POOLS as $stepPool) {
+            $data = $this->prepareBuilder($stepPool::whereOlderThan($time))->get();
 
-        for ($i = 0; $i < count($results); $i++) {
-            $results[$i]->project_url = action(DetailController::routeMethod('getIndex'), [
-                'project_id' => $results[$i]->project_id
-            ]);
-            $results[$i]->resource_url = action(ResourceDetailController::routeMethod('getIndex'), [
-                'project_id' => $results[$i]->project_id,
-                'resource_id' => $results[$i]->resource_id
-            ]);
+            if ($data->count() > 0) {
+                array_push($results, ...$data);
+            }
         }
 
+        $this->addUrls($results);
         return $results;
-    }
-
-    /**
-     * @param Builder $builder
-     * @return Builder
-     */
-    private function prepareBuilder(Builder $builder): Builder {
-        return $builder->whereActiveIn([1, 2, 5])->with([
-            'project' => function (BelongsTo $query) {
-                $query->with('eshopTypeName')->select(['id', 'user_id', 'eshop_type_id']);
-            },
-            'resource' => function (BelongsTo $query) {
-                $query->select(['id', 'name']);
-            }
-        ])->select(['active', 'ttl', 'project_id', 'resource_id', 'unique']);
     }
 }
