3dfdc78cfba1abf4c9c4487682c2e369815525ad
lukas <lukas.kielar@monkeydata.com>
2018-09-19 08:51:14 +0200

    Switched resources in error state back to only include resources with active = 3.

--- app/Http/Controllers/Open/ImportFlow/Table/ResourcesInErrorStateController.php
+++ app/Http/Controllers/Open/ImportFlow/Table/ResourcesInErrorStateController.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\Project\DetailController;
 use App\Http\Controllers\Project\Resource\DetailController as ResourceDetailController;
 use App\Model\ResourceSetting;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Routing\Controller;
-use Monkey\Constants\ImportFlow\Resource\ResourceSetting as ResourceSettingsConstants;
 
 /**
  * Class ResourcesInErrorState
@@ -27,7 +25,8 @@ class ResourcesInErrorStateController extends Controller {
             ->join('client', 'client.user_id', '=', 'user.id')
             ->whereNull('client.deleted_at')
             ->leftJoin('eshop_type', 'eshop_type_id', '=', 'eshop_type.id')
-            ->with(['resourceName'])->where('resource_setting.active', '>', 2)
+            ->with(['resourceName'])
+            ->where('resource_setting.active', 3)
             ->orderBy('resource_setting.updated_at', 'desc')
             ->get(['resource_setting.active', 'user.id', 'eshop_type_id', 'project_id', 'resource_id', 'ttl', 'eshop_type.name']);
 
