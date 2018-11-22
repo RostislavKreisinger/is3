3bf01d2c329593ddc4e2e8ee3f7d35ab39e7b49b
lukas <lukas.kielar@monkeydata.com>
2018-11-22 15:22:48 +0100

    Added migration that adds soft deletes to import shutdown log and model that works with it.

--- app/Http/Controllers/Homepage/ImportShutdownLogController.php
+++ app/Http/Controllers/Homepage/ImportShutdownLogController.php
@@ -6,52 +6,32 @@ namespace App\Http\Controllers\Homepage;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Project\DetailController;
 use App\Http\Controllers\Project\Resource\DetailController as ResourceDetailController;
-use Monkey\Connections\Extension\LaravelMySqlConnection;
-use Monkey\Connections\MDDatabaseConnections;
+use App\Model\ShutdownLog;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Class ImportShutdownLogController
  * @package App\Http\Controllers\Homepage
  */
 class ImportShutdownLogController extends Controller {
-    const SHUTDOWN_LOG_TABLE = 'if_shutdown_log';
-
     /**
-     * @var LaravelMySqlConnection $logsConnection
-     */
-    private $logsConnection;
-
-    /**
-     * @return array|static[]
+     * @return Collection|ShutdownLog[]
      */
     public function getIndex() {
-        if ($this->getLogsConnection()->getSchemaBuilder()->hasTable(self::SHUTDOWN_LOG_TABLE)) {
-            $logs = $this->getLogsConnection()->table(self::SHUTDOWN_LOG_TABLE)
-                ->orderBy('datetime', 'desc')
-                ->get();
+        $logs = ShutdownLog::query()
+            ->orderBy('datetime', 'desc')
+            ->get();
 
-            for ($i = 0; $i < count($logs); $i++) {
-                $logs[$i]->project_url = action(DetailController::routeMethod('getIndex'), [
-                    'project_id' => $logs[$i]->project_id
-                ]);
-                $logs[$i]->resource_url = action(ResourceDetailController::routeMethod('getIndex'), [
-                    'project_id' => $logs[$i]->project_id,
-                    'resource_id' => $logs[$i]->resource_id
-                ]);
-            }
+        $logs->map(function (ShutdownLog $log) {
+            $log->project_url = action(DetailController::routeMethod('getIndex'), [
+                'project_id' => $log->project_id
+            ]);
+            $log->resource_url = action(ResourceDetailController::routeMethod('getIndex'), [
+                'project_id' => $log->project_id,
+                'resource_id' => $log->resource_id
+            ]);
+        });
 
-            return $logs;
-        }
-    }
-
-    /**
-     * @return LaravelMySqlConnection
-     */
-    public function getLogsConnection(): LaravelMySqlConnection {
-        if (is_null($this->logsConnection)) {
-            $this->logsConnection = MDDatabaseConnections::getLogsConnection();
-        }
-
-        return $this->logsConnection;
+        return $logs;
     }
 }
---
+++ app/Model/ShutdownLog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model;
+
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Monkey\Connections\Extension\LaravelMySqlConnection;
+use Monkey\Connections\MDDatabaseConnections;
+
+/**
+ * Class ShutdownLog
+ * @package App\Model
+ */
+class ShutdownLog extends Model {
+    use SoftDeletes;
+
+    /**
+     * @var string $table
+     */
+    protected $table = 'if_shutdown_log';
+
+    /**
+     * @return Connection|LaravelMySqlConnection
+     */
+    public function getConnection() {
+        return MDDatabaseConnections::getLogsConnection();
+    }
+}
