e38f5947a2ed0e51367d3773ecf183247e061817
lukas <lukas.kielar@monkeydata.com>
2019-03-15 09:37:41 +0100

    Added develop token to graphs.

--- app/Http/Controllers/Homepage/BaseController.php
+++ app/Http/Controllers/Homepage/BaseController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Homepage;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\IndexController;
 use Monkey\Config\Application\ProjectEndpointBaseUrl;
+use Monkey\Connections\MDDatabaseConnections;
 
 /**
  * Class BaseController
@@ -15,5 +16,22 @@ class BaseController extends Controller {
     public function getIndex() {
         $this->getView()->addParameter('baseUrl', action(IndexController::routeMethod('getIndex')));
         $this->getView()->addParameter('baseApiUrl', ProjectEndpointBaseUrl::getInstance()->getImportSupportApiUrl());
+        $this->getView()->addParameter('apiToken', $this->getApiToken());
+    }
+
+    /**
+     * @return string
+     */
+    private function getApiToken(): string {
+        $tokenRecord = MDDatabaseConnections::getAccountsConnection()->table('access_token')
+            ->orderBy('expires_at', 'desc')
+            ->where('user_id', 15300)
+            ->first(['access_token']);
+
+        if (empty($tokenRecord)) {
+            return '';
+        }
+
+        return $tokenRecord->access_token;
     }
 }
