f46eea628a09b6052b3e80beec51a7233fe1c1ac
RostaK <rostislav.kreisinger@monkeydata.cz>
2019-03-18 13:12:44 +0100

    default setup of limits

--- database/seeds/MonitoringAttributesSeeder.php
+++ database/seeds/MonitoringAttributesSeeder.php
@@ -16,20 +16,20 @@ class MonitoringAttributesSeeder extends Seeder
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
             ->table("if_monitoring_attribute")->insert(["name"=>"count_long_run_time_flows","criticalValue"=>100]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_import_time_to_start","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_import_time_to_start","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_etl_time_to_start","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_etl_time_to_start","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_calc_time_to_start","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_calc_time_to_start","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_output_time_to_start","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_output_time_to_start","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_import_run_time","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_import_run_time","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_etl_run_time","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_etl_run_time","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_calc_run_time","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_calc_run_time","criticalValue"=>1800]);
         \Monkey\Connections\MDDatabaseConnections::getImportSupportConnection()
-            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_output_run_time","criticalValue"=>3600]);
+            ->table("if_monitoring_attribute")->insert(["name"=>"long_average_output_run_time","criticalValue"=>1800]);
     }
 }
